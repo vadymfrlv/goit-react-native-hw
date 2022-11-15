1b9a6abf296d10473b625434c0453c6e48d1cc3a
Vadym Frolov <vdfroloff@gmail.com>
2022-11-15 13:05:18 +0200

    upds

--- src/screens/main/CreatePostsScreen.jsx
+++ src/screens/main/CreatePostsScreen.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import {
   StyleSheet,
   Text,
@@ -13,20 +13,26 @@ import {
   TouchableWithoutFeedback,
 } from 'react-native';
 import { Octicons, MaterialIcons, EvilIcons } from '@expo/vector-icons';
-
 import { Camera, CameraType } from 'expo-camera';
 import * as MediaLibrary from 'expo-media-library';
 import * as Location from 'expo-location';
 
 const CreatePostsScreen = ({ navigation }) => {
+  const initialPostData = {
+    photo: '',
+    description: '',
+    location: '',
+  };
+  const [postData, setPostData] = useState(initialPostData);
+
   const [type, setType] = useState(CameraType.back);
   const [camera, setCamera] = useState(null);
   const [photo, setPhoto] = useState(null);
   const [location, setLocation] = useState(null);
-  // const [address, setAddress] = useState(null);
-  // const [addressValue, setAddressValue] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [addressValue, setAddressValue] = useState(null);
   const [title, setTitle] = useState('');
-  const [keyboardShown, setKeyboardShown] = useState(false);
+  const [isShowKeyboard, setIsShowKeyboard] = useState(false);
 
   // const { userId, name } = useSelector(state => state.auth);
 
@@ -77,93 +83,86 @@ const CreatePostsScreen = ({ navigation }) => {
   };
 
   const keyboardHide = () => {
-    setKeyboardShown(false);
+    setIsShowKeyboard(false);
     Keyboard.dismiss();
   };
 
   return (
     <View style={styles.container}>
       <TouchableWithoutFeedback onPress={keyboardHide}>
-        <KeyboardAvoidingView behavior={Platform.OS === 'ios' && 'padding'}>
-          <View>
-            {photo && location ? (
-              <View style={styles.photoContainer}>
-                <TouchableOpacity style={styles.cancelPhotoButton} onPress={cancelPhoto}>
-                  <MaterialIcons name="close" size={40} color="#BDBDBD" />
-                </TouchableOpacity>
-                <Image style={styles.photo} source={{ uri: photo }} />
-              </View>
+        <KeyboardAvoidingView behavior={Platform.OS == 'ios' ? 'padding' : 'height'}>
+          {photo ? (
+            <View style={styles.photoContainer}>
+              <TouchableOpacity style={styles.retakePhotoBtn} onPress={cancelPhoto}>
+                <Octicons name="x" size={45} color="#F6F6F6" />
+              </TouchableOpacity>
+              <Image style={styles.photo} source={{ uri: photo }} />
+            </View>
+          ) : (
+            <Camera style={styles.camera} type={type} flashMode="auto" ref={ref => setCamera(ref)}>
+              <TouchableOpacity style={styles.cameraTypeButton} onPress={toggleCameraType}>
+                <Octicons name="sync" size={30} color="#F6F6F6" />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.cameraSnapButton} onPress={takePhoto}>
+                <Octicons name="issue-opened" size={50} color="#F6F6F6" />
+              </TouchableOpacity>
+            </Camera>
+          )}
+          <View style={{ marginTop: 8, marginHorizontal: 16 }}>
+            {photo ? (
+              <Text
+                style={{
+                  fontFamily: 'Roboto-Regular',
+                  fontSize: 16,
+                  color: '#BDBDBD',
+                }}
+              >
+                Edit photo
+              </Text>
             ) : (
-              <Camera
-                style={styles.camera}
-                type={type}
-                flashMode="auto"
-                ref={ref => setCamera(ref)}
+              <Text
+                style={{
+                  fontFamily: 'Roboto-Regular',
+                  fontSize: 16,
+                  color: '#BDBDBD',
+                }}
               >
-                <TouchableOpacity style={styles.cameraTypeButton} onPress={toggleCameraType}>
-                  {type === CameraType.back ? (
-                    <MaterialIcons name="camera-front" size={32} color="#BDBDBD" />
-                  ) : (
-                    <MaterialIcons name="camera-rear" size={32} color="#BDBDBD" />
-                  )}
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cameraSnapButton} onPress={takePhoto}>
-                  <MaterialIcons name="camera-alt" size={32} color="#BDBDBD" />
-                </TouchableOpacity>
-              </Camera>
+                Take a photo
+              </Text>
             )}
-            <View style={{ marginTop: 8, marginHorizontal: 16 }}>
-              {photo ? (
-                <Text
-                  style={{
-                    fontFamily: 'Roboto-Regular',
-                    fontSize: 16,
-                    color: '#BDBDBD',
-                  }}
-                >
-                  Edit photo
-                </Text>
-              ) : (
-                <Text
-                  style={{
-                    fontFamily: 'Roboto-Regular',
-                    fontSize: 16,
-                    color: '#BDBDBD',
-                  }}
-                >
-                  Upload photo
-                </Text>
-              )}
-            </View>
-            <View style={styles.inputWrapper}>
-              <TextInput
-                style={{ ...styles.input, marginBottom: 16 }}
-                placeholder="Name..."
-                onFocus={() => setKeyboardShown(true)}
-                value={title}
-                onChangeText={setTitle}
-              />
-              <TextInput
-                style={{ ...styles.input, paddingLeft: 30 }}
-                placeholder="Location..."
-                editable={false}
-                onFocus={() => setKeyboardShown(true)}
-                value={addressValue && `${addressValue?.city}, ${addressValue?.name}`}
-              />
-              <TouchableOpacity
-                style={{ position: 'absolute', top: 76, left: 5 }}
-                activeOpacity={0.8}
-                onPress={() => setAddressValue(address)}
-              >
-                {/* <EvilIcons name="location" size={40} color="#BDBDBD" /> */}
-                <Octicons name="location" size={40} color="#BDBDBD" />
-              </TouchableOpacity>
-            </View>
-            <View>
-              <TouchableOpacity style={styles.sendButton} activeOpacity={0.8} onPress={sendPost}>
-                <Text style={styles.sendButtonTitle}>Share</Text>
-              </TouchableOpacity>
-            </View>
+          </View>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={{ ...styles.input, marginBottom: 16 }}
+              placeholder="Description..."
+              onFocus={() => setIsShowKeyboard(true)}
+              value={postData.description}
+              onChangeText={value =>
+                setPostData(prevPostData => ({ ...prevPostData, description: value }))
+              }
+            />
+            <TextInput
+              style={{ ...styles.input, paddingLeft: 28 }}
+              placeholder="Location..."
+              editable={false}
+              onChangeText={value =>
+                setPostData(prevPostData => ({ ...prevPostData, location: value }))
+              }
+              onFocus={() => setIsShowKeyboard(true)}
+              value={addressValue && `${addressValue?.city}, ${addressValue?.country} `}
+            />
+            <TouchableOpacity
+              style={{ position: 'absolute', top: 76, left: 5 }}
+              activeOpacity={0.8}
+              onPress={() => setAddressValue(address)}
+            >
+              <Octicons name="location" size={25} color="#BDBDBD" />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.sendButton} activeOpacity={0.8} onPress={sendPost}>
+              <Text style={styles.sendButtonTitle}>Share</Text>
+            </TouchableOpacity>
           </View>
         </KeyboardAvoidingView>
       </TouchableWithoutFeedback>
@@ -177,58 +176,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'flex-end',
+    justifyContent: 'flex-start',
   },
   camera: {
     position: 'relative',
-    height: 440,
+    height: 240,
+    marginTop: 32,
     marginHorizontal: 16,
     borderRadius: 8,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
     overflow: 'hidden',
   },
   cameraTypeButton: {
     position: 'absolute',
     top: 10,
-    right: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 40,
-    height: 40,
-    borderRadius: 50,
-    opacity: 0.5,
+    right: 13,
+    opacity: 0.7,
   },
   cameraSnapButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 60,
-    height: 60,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 50,
-    opacity: 0.5,
+    marginBottom: 20,
+    opacity: 0.7,
   },
   photoContainer: {
     position: 'relative',
-    height: 440,
     marginHorizontal: 16,
+    height: 240,
     marginTop: 32,
-    borderRadius: 8,
   },
   photo: {
-    height: 440,
+    position: 'relative',
+    height: 240,
     borderRadius: 8,
     resizeMode: 'cover',
   },
-  cancelPhotoButton: {
+  retakePhotoBtn: {
     position: 'absolute',
-    top: 10,
-    right: 10,
-    zIndex: 100,
+    top: 2,
+    right: 11,
+    opacity: 0.7,
+    zIndex: 1,
   },
   inputWrapper: {
     position: 'relative',
-    marginTop: 20,
+    marginTop: 32,
   },
   input: {
     height: 50,
